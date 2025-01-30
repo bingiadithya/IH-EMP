64ce531e3d6dbcb53830512e4b582e5470e196ff
adithya <bingiadithya@gmail.com>
2025-01-30 19:31:21 +0530

    Backend

---
+++ STS-Workspace/springboot-backend/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into employees(id,first_name,last_name,email) 
+		values(1,'Alan','Morries','alan@gmail.com');
+
+		insert into employees(id,first_name,last_name,email) 
+		values(2,'Erick','Anderson','eric@yahoo.com');
